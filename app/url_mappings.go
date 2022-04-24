8e725a34e0e1918f2bfd2daba29eab600ba53cf5
kousukef <kousukef123@yahoo.co.jp>
2022-04-24 15:01:11 +0900

    add update user api

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -11,4 +11,6 @@ func mapUrls() {
 	router.POST("/users", users.CreateUser)
 	router.GET("/users/search", users.SearchUser)
 	router.GET("/users/:user_id", users.GetUser)
+	router.PUT("/users/:user_id", users.UpdateUser)
+	router.PATCH("/users/:user_id", users.UpdateUser)
 }
--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -45,3 +45,30 @@ func CreateUser(c *gin.Context) {
 }
 
 func SearchUser(c *gin.Context) {}
+
+func UpdateUser(c *gin.Context) {
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("Invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	user.Id = userId
+
+	isPartial := c.Request.Method == http.MethodPatch
+
+	result, err := services.UpdateUser(isPartial, user)
+	if err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+	c.JSON(http.StatusOK, result)
+}
